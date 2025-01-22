135013a6298bd8b4e304ba4a9853a80f7899f873
jth <juthomas@tandemdiabetes.com>
2025-01-22 14:50:04 +0100

    lib.rs: Register a module pass instead + POC call graph

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,21 @@
 // See https://github.com/banach-space/llvm-tutor/blob/main/HelloWorld/HelloWorld.cpp
 // for a more detailed explanation.
 
-use llvm_plugin::inkwell::values::{CallSiteValue, FunctionValue};
+use llvm_plugin::inkwell::module::Module;
+use llvm_plugin::inkwell::values;
+use llvm_plugin::inkwell::values::CallSiteValue;
 use llvm_plugin::{
-    FunctionAnalysisManager, LlvmFunctionPass, PassBuilder, PipelineParsing, PreservedAnalyses,
+    LlvmModulePass, ModuleAnalysisManager, PassBuilder, PipelineParsing, PreservedAnalyses,
 };
 
-#[llvm_plugin::plugin(name = "HelloWorld", version = "0.1")]
+use petgraph::dot::{Config, Dot};
+use petgraph::Graph;
+
+#[llvm_plugin::plugin(name = "plugin_name", version = "0.1")]
 fn plugin_registrar(builder: &mut PassBuilder) {
-    builder.add_function_pipeline_parsing_callback(|name, manager| {
+    builder.add_module_pipeline_parsing_callback(|name, manager| {
         if name == "hello-world" {
-            manager.add_pass(HelloWorldPass);
+            manager.add_pass(CustomPass);
             PipelineParsing::Parsed
         } else {
             PipelineParsing::NotParsed
@@ -18,42 +23,57 @@ fn plugin_registrar(builder: &mut PassBuilder) {
     });
 }
 
-struct HelloWorldPass;
-impl LlvmFunctionPass for HelloWorldPass {
-    fn run_pass(
-        &self,
-        function: &mut FunctionValue,
-        _manager: &FunctionAnalysisManager,
-    ) -> PreservedAnalyses {
-        eprintln!("(llvm-tutor) -------------");
-        eprintln!("(llvm-tutor) Hello from: {:?}", function.get_name());
-        eprintln!(
-            "(llvm-tutor)   number of arguments: {}",
-            function.count_params()
-        );
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
+struct Function {
+    name: String,
+}
 
-        /* Equivalent code in C++
+impl From<values::FunctionValue<'_>> for Function {
+    fn from(item: values::FunctionValue) -> Self {
+        Self {
+            name: item.get_name().to_str().expect("").into(),
+        }
+    }
+}
 
-           for (auto &bb : function) {
-               for (auto &instruction : bb) {
-                   if (CallInst *callInst = dyn_cast<CallInst>(&instruction)) {
-                       if (Function *calledFunction = callInst->getCalledFunction()) {
-                           std::cerr << calledFunction->getName();
+struct CustomPass;
+impl LlvmModulePass for CustomPass {
+    fn run_pass(&self, module: &mut Module, _manager: &ModuleAnalysisManager) -> PreservedAnalyses {
+        let mut omega_tree = Graph::<Function, ()>::new();
+
+        for function in module.get_functions() {
+            /* Equivalent code in C++
+
+               for (auto &bb : function) {
+                   for (auto &instruction : bb) {
+                       if (CallInst *callInst = dyn_cast<CallInst>(&instruction)) {
+                           if (Function *calledFunction = callInst->getCalledFunction()) {
+                               std::cerr << calledFunction->getName();
+                           }
                        }
                    }
                }
-           }
-        */
-        for basic_block in function.get_basic_blocks() {
-            for instruction in basic_block.get_instructions() {
-                if let Ok(call_site_value) = CallSiteValue::try_from(instruction) {
-                    eprintln!(
-                        "(llvm-tutor)   callees: {:?}",
-                        call_site_value.get_called_fn_value().get_name()
-                    );
+            */
+
+            let current_function = omega_tree.add_node(function.into());
+
+            for basic_block in function.get_basic_blocks() {
+                for instruction in basic_block.get_instructions() {
+                    if let Ok(call_site_value) = CallSiteValue::try_from(instruction) {
+                        let callee =
+                            omega_tree.add_node(call_site_value.get_called_fn_value().into());
+                        omega_tree.add_edge(current_function, callee, ());
+
+                        //let
+                    }
                 }
             }
         }
+        eprintln!(
+            "{:?}",
+            Dot::with_config(&omega_tree, &[Config::EdgeNoLabel])
+        );
+
         PreservedAnalyses::All
     }
 }
