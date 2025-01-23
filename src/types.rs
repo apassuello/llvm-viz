36c0e157723f23987741d0c8943919cfb01cd466
jth <juthomas@tandemdiabetes.com>
2025-01-23 18:05:43 +0100

    types.rs: Add `graph_from_json()`

--- src/types.rs
+++ src/types.rs
@@ -1,14 +1,14 @@
 use llvm_plugin::inkwell::values;
 use petgraph::graph::NodeIndex;
 use petgraph::Graph;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
 use std::error::Error;
 use std::fs::File;
-use std::io::Write;
+use std::io::{Read, Write};
 use std::path::Path;
 
-#[derive(Debug, Clone, PartialEq, PartialOrd, Serialize)]
+#[derive(Debug, Clone, PartialEq, PartialOrd, Serialize, Deserialize)]
 pub struct Function {
     name: String,
 }
@@ -26,6 +26,13 @@ pub fn get_index_or_insert<E>(graph: &mut Graph<Function, E>, node: Function) ->
         .node_indices()
         .find(|ix| graph[*ix].name == node.name)
         .unwrap_or_else(|| graph.add_node(node))
+}
+
+pub fn graph_from_json(path: &Path) -> Result<Graph<Function, ()>, Box<dyn Error>> {
+    let mut file = File::open(path)?;
+    let mut file_content = String::new();
+    file.read_to_string(&mut file_content)?;
+    Ok(serde_json::from_str(&file_content.clone())?)
 }
 
 pub fn graph_to_json(path: &Path, data: &Graph<Function, ()>) -> Result<(), Box<dyn Error>> {
