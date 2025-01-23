60766fcc13d33173d240b0db428f8ba40eca56d9
jth <juthomas@tandemdiabetes.com>
2025-01-23 18:07:04 +0100

    viz/main.rs: Add sample app that load json file

--- src/viz/main.rs
+++ src/viz/main.rs
@@ -1,4 +1,12 @@
-fn main() {
-    println!("Hello, World!");
-}
+use std::path::Path;
 
+use petgraph::dot::{Config, Dot};
+//use petgraph::Graph;
+
+use llvm_viz::types;
+
+fn main() {
+    let g = types::graph_from_json(Path::new("omega_tree.json")).expect("");
+
+    println!("{:?}", Dot::with_config(&g, &[Config::EdgeNoLabel]));
+}
