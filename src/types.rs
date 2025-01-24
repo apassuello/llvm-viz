eb00f45ba10037f501a3b58f9ef3cc5da619c44e
jth <juthomas@tandemdiabetes.com>
2025-01-24 16:08:25 +0100

    types.rs: Add boilerplate for `append_graph()`

--- src/types.rs
+++ src/types.rs
@@ -28,6 +28,13 @@ pub fn get_index_or_insert<E>(graph: &mut Graph<Function, E>, node: Function) ->
         .unwrap_or_else(|| graph.add_node(node))
 }
 
+pub fn append_graph<E>(
+    source: &mut Graph<Function, E>,
+    dest: &mut Graph<Function, E>,
+) -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
+
 pub fn graph_from_json(path: &Path) -> Result<Graph<Function, ()>, Box<dyn Error>> {
     let mut file = File::open(path)?;
     let mut file_content = String::new();
@@ -43,6 +50,53 @@ pub fn graph_to_json(path: &Path, data: &Graph<Function, ()>) -> Result<(), Box<
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    // Credit: https://github.com/petgraph/petgraph/issues/199#issuecomment-484077775
+    fn graph_eq<N, E, Ty, Ix>(
+        a: &petgraph::Graph<N, E, Ty, Ix>,
+        b: &petgraph::Graph<N, E, Ty, Ix>,
+    ) -> bool
+    where
+        N: PartialEq,
+        E: PartialEq,
+        Ty: petgraph::EdgeType,
+        Ix: petgraph::graph::IndexType + PartialEq,
+    {
+        let a_ns = a.raw_nodes().iter().map(|n| &n.weight);
+        let b_ns = b.raw_nodes().iter().map(|n| &n.weight);
+        let a_es = a
+            .raw_edges()
+            .iter()
+            .map(|e| (e.source(), e.target(), &e.weight));
+        let b_es = b
+            .raw_edges()
+            .iter()
+            .map(|e| (e.source(), e.target(), &e.weight));
+        a_ns.eq(b_ns) && a_es.eq(b_es)
+    }
+
+    #[test]
+    fn merge_two_nodes() {
+        let mut g = Graph::<_, ()>::new();
+        _ = g.add_node(Function {
+            name: "fn1".to_owned(),
+        });
+        _ = g.add_node(Function {
+            name: "fn2".to_owned(),
+        });
+
+        let mut g1 = Graph::<_, ()>::new();
+        _ = g1.add_node(Function {
+            name: "fn1".to_owned(),
+        });
+        let mut g2 = Graph::<_, ()>::new();
+        _ = g2.add_node(Function {
+            name: "fn2".to_owned(),
+        });
+        append_graph(&mut g1, &mut g2).unwrap();
+
+        assert!(graph_eq(&g, &g1));
+    }
 
     #[test]
     fn insert_new_node_twice() {
