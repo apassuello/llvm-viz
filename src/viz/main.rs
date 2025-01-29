8eea880e15828d712642343fb1c35822286e0fc2
jth <juthomas@tandemdiabetes.com>
2025-01-29 13:19:28 +0100

    viz: Make hello world example with GUI

--- src/viz/main.rs
+++ src/viz/main.rs
@@ -1,12 +1,33 @@
 use std::path::Path;
 
+extern crate piston_window;
 use petgraph::dot::{Config, Dot};
-//use petgraph::Graph;
+use piston_window::*;
 
 use llvm_viz::types;
 
 fn main() {
     let g = types::graph_from_json(Path::new("omega_tree.json")).expect("");
-
     println!("{:?}", Dot::with_config(&g, &[Config::EdgeNoLabel]));
+
+    let nodes: Vec<_> = g
+        .raw_nodes()
+        .iter()
+        .map(|n| (n.weight.name.clone(), [100.0, 100.0, 100.0, 100.0]))
+        .collect();
+
+    let mut window: PistonWindow = WindowSettings::new("Hello Piston!", [640, 480])
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |c, g, _device| {
+            clear([1.0; 4], g);
+
+            for node in nodes.clone() {
+                rectangle([0.5, 0.7, 0.0, 1.0], node.1, c.transform, g);
+            }
+        });
+    }
 }
