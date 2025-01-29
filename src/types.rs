6724d78574a3d17f9890a3ff5df5c5268f2d2146
jth <juthomas@tandemdiabetes.com>
2025-01-29 13:18:56 +0100

    types.rs: Make member `name` pub

--- src/types.rs
+++ src/types.rs
@@ -10,7 +10,7 @@ use std::path::Path;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Function {
-    name: String,
+    pub name: String,
 }
 
 impl From<values::FunctionValue<'_>> for Function {
