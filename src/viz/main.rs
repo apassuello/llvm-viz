f29f5fe4c7706c654b79f21e7d1dc722c6dee953
julien <ju@pop-os.localdomain>
2025-02-04 18:19:27 +0100

    viz: Use bevy + register each `Function` as a Entity/Component

--- src/viz/main.rs
+++ src/viz/main.rs
@@ -1,20 +1,21 @@
-use std::path::Path;
-
+use bevy::prelude::*;
 use petgraph::dot::{Config, Dot};
+
+use std::path::Path;
 
 use llvm_viz::types;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Component)]
 struct Rectangle {
     name: String,
     coords: [f64; 4],
 }
 
-fn main() {
+fn add_rectangles(mut commands: Commands) {
     let g = types::graph_from_json(Path::new("omega_tree.json")).expect("");
     println!("{:?}", Dot::with_config(&g, &[Config::EdgeNoLabel]));
 
-    let nodes: Vec<_> = g
+    for r in g
         .raw_nodes()
         .iter()
         .enumerate()
@@ -22,5 +23,21 @@ fn main() {
             name: n.weight.name.clone(),
             coords: [i as f64 * 150.0 + 50 as f64, 100.0, 100.0, 100.0],
         })
-        .collect();
+        .collect::<Vec<_>>()
+    {
+        commands.spawn(r);
+    }
+}
+
+fn greet_rectantles(query: Query<&Rectangle>) {
+    for rectangle in &query {
+        println!("hello {}!", rectangle.name);
+    }
+}
+
+fn main() {
+    App::new()
+        .add_systems(Startup, add_rectangles)
+        .add_systems(Update, greet_rectantles)
+        .run();
 }
