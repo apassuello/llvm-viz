96e3f96fc021be3b16a7d2c458a6a7e5361bcad1
jth <juthomas@tandemdiabetes.com>
2025-02-04 13:54:57 +0100

    viz: Fix compilation

--- src/viz/main.rs
+++ src/viz/main.rs
@@ -1,8 +1,6 @@
 use std::path::Path;
 
-extern crate piston_window;
 use petgraph::dot::{Config, Dot};
-use piston_window::*;
 
 use llvm_viz::types;
 
@@ -25,20 +23,4 @@ fn main() {
             coords: [i as f64 * 150.0 + 50 as f64, 100.0, 100.0, 100.0],
         })
         .collect();
-
-    let mut window: PistonWindow = WindowSettings::new("Hello Piston!", [640, 480])
-        .exit_on_esc(true)
-        .build()
-        .unwrap();
-
-    while let Some(e) = window.next() {
-        window.draw_2d(&e, |c, g, _device| {
-            clear([1.0; 4], g);
-
-            for node in nodes.clone() {
-                // todo: Also display node's name
-                rectangle([0.5, 0.7, 0.0, 1.0], node.coords, c.transform, g);
-            }
-        });
-    }
 }
