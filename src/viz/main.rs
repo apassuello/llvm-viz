41f7d279ee7cc4bd0bb692d75997db85de5d6200
jth <juthomas@tandemdiabetes.com>
2025-02-06 13:57:55 +0100

    viz: Add a movable camera

--- src/viz/main.rs
+++ src/viz/main.rs
@@ -13,12 +13,12 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(FpsOverlayPlugin::default())
-        .add_systems(Startup, (setup_scene, setup_camera))
-        // .add_systems(Update, (move_player, update_camera).chain())
+        .add_systems(Startup, (setup_player, setup_camera))
+        .add_systems(Update, move_camera)
         .run();
 }
 
-fn setup_scene(
+fn setup_player(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
@@ -28,10 +28,40 @@ fn setup_scene(
         Player,
         Mesh2d(meshes.add(Rectangle::new(100., 30.))),
         MeshMaterial2d(materials.add(Color::srgb(1., 1., 1.))),
-        // Transform::from_xyz(0., 0., 2.),
+        Transform::from_xyz(0., 0., 2.),
     ));
 }
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn((Camera2d, Camera::default()));
+}
+
+fn move_camera(
+    mut camera: Query<&mut Transform, (With<Camera2d>, Without<Player>)>,
+    kb_input: Res<ButtonInput<KeyCode>>,
+) {
+    let Ok(mut camera) = camera.get_single_mut() else {
+        return;
+    };
+
+    let mut direction = Vec2::ZERO;
+
+    if kb_input.pressed(KeyCode::KeyW) {
+        direction.y += 1.;
+    }
+
+    if kb_input.pressed(KeyCode::KeyS) {
+        direction.y -= 1.;
+    }
+
+    if kb_input.pressed(KeyCode::KeyA) {
+        direction.x -= 1.;
+    }
+
+    if kb_input.pressed(KeyCode::KeyD) {
+        direction.x += 1.;
+    }
+
+    let move_delta = direction.normalize_or_zero();
+    camera.translation += move_delta.extend(0.);
 }
