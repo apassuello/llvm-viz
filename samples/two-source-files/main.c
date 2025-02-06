56f5c0af2e4c0e414682bc150c736a8d0d353560
apa <apa@debian.apa>
2025-02-06 11:44:55 +0100

    [samples/two-source-files/main] Added mult function

--- samples/two-source-files/main.c
+++ samples/two-source-files/main.c
@@ -1,5 +1,10 @@
 #include "math.h"
 
+extern int32_t add(int32_t a, int32_t b);
+extern int32_t mult(int32_t a, int32_t b) {
+        return a * b;
+}
+
 int main(int argc, char *argv[])
 {
         return add(1,1);
